c3720bcdf5e92ba55443bbd0e7b55f9dbeeb92a3
Daniele Tabanella <daniele.tabanella@gmail.com>
2023-07-03 15:17:43 +0200

    feat(update script and librairies): update

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,12 @@ const char* ON[] = {
   "0FF00FFF1001",
   "0FF00FF10001",
   "0FF00F1F0001",
-  "0FF001FF0001"
+  "0FF001FF0001",
+  "0000FFFF010130",
+  "0000FFFF100130",
+  "0000FFF1000130",
+  "0000FF1F000130",
+  "0000F1FF000130"
 };
 
 const char* getStates[] = {
@@ -26,7 +31,12 @@ const char* getStates[] = {
   "lights/zap2/getState",
   "lights/zap3/getState",
   "lights/zap4/getState",
-  "lights/zap5/getState"
+  "lights/zap5/getState",
+  "lights/zap6/getState",
+  "lights/zap7/getState",
+  "lights/zap8/getState",
+  "lights/zap9/getState",
+  "lights/zap10/getState"
 };
 
 const char* setStates[] = {
@@ -34,7 +44,12 @@ const char* setStates[] = {
   "lights/zap2/setState",
   "lights/zap3/setState",
   "lights/zap4/setState",
-  "lights/zap5/setState"
+  "lights/zap5/setState",
+  "lights/zap6/setState",
+  "lights/zap7/setState",
+  "lights/zap8/setState",
+  "lights/zap9/setState",
+  "lights/zap10/setState"
 };
 
 const char* OFF[] = {
@@ -42,7 +57,12 @@ const char* OFF[] = {
   "0FF00FFF1010",
   "0FF00FF10010",
   "0FF00F1F0010",
-  "0FF001FF0010"
+  "0FF001FF0010",
+  "0000FFFF011030",
+  "0000FFFF101030",
+  "0000FFF1001030",
+  "0000FF1F001030",
+  "0000F1FF001030"
 };
 
 // MQTT
@@ -50,12 +70,12 @@ const char* OFF[] = {
 WiFiClient espClient;
 PubSubClient MQTTclient(espClient);
 
-#define mqtt_server "homebridge"
+#define mqtt_server "home.local"
 
 unsigned long startMQTTmillis;
 unsigned long currentMQTTmillis;
 const unsigned long MQTTdelay = 5000;
-const size_t capacity = JSON_OBJECT_SIZE(4);
+const size_t capacity = JSON_OBJECT_SIZE(10);
 
 void MQTTcallback(char* topic, byte* payload, unsigned int length) {
 
@@ -69,7 +89,7 @@ void MQTTcallback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  for (size_t i = 0; i < 5; i++) {          // loop setStates
+  for (size_t i = 0; i < 10; i++) {          // loop setStates
     bool test = !strcmp(setStates[i], topic);
           // test if in `i` topics are identic
     Serial.println(setStates[i]);
@@ -104,6 +124,11 @@ boolean reconnect() {
     MQTTclient.subscribe("lights/zap3/setState");
     MQTTclient.subscribe("lights/zap4/setState");
     MQTTclient.subscribe("lights/zap5/setState");
+    MQTTclient.subscribe("lights/zap6/setState");
+    MQTTclient.subscribe("lights/zap7/setState");
+    MQTTclient.subscribe("lights/zap8/setState");
+    MQTTclient.subscribe("lights/zap9/setState");
+    MQTTclient.subscribe("lights/zap10/setState");
 
   }
 
@@ -179,14 +204,14 @@ void loop() {
 
   if (receiver.available()) {
 
-    for (size_t i = 0; i < 4; i++) {
+    for (size_t i = 0; i < 10; i++) {
       bool test = testTristate(receiver.getReceivedValue(), receiver.getReceivedBitlength(), ON[i]);
       if (test) sendLightStatusOverMQTT(getStates[i], true);
       Serial.print(i);
       Serial.print(test);
     }
 
-    for (size_t i = 0; i < 4; i++) {
+    for (size_t i = 0; i < 10; i++) {
       bool test = testTristate(receiver.getReceivedValue(), receiver.getReceivedBitlength(), OFF[i]);
       if (test) sendLightStatusOverMQTT(getStates[i], false);
       Serial.print(i);
